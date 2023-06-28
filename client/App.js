4a2be4cccc1da66e3550de5126de776a4b9cd13c
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2023-06-28 11:00:40 -0500

    Add styles with class names

--- client/App.js
+++ client/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 function App(){
     return(
         <div>
-            Hello!!!! I'm in React!!
+            <h1>Hello!!!! I'm in React!!</h1>
+            <button className="btn btn-danger">This is a button!</button>
         </div>
     )
 }
